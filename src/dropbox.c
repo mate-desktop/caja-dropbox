e214b332395adbc77ad596218895a4834cad434d
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2012-03-27 03:03:55 +0200

    import nautilus dropbox 0.7.1

---
+++ src/dropbox.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2008 Evenflow, Inc.
+ *
+ * dropbox.c
+ * Nautilus module registering functions for the Dropbox extension.
+ *
+ * This file is part of nautilus-dropbox.
+ *
+ * nautilus-dropbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nautilus-dropbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with nautilus-dropbox.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib-object.h>
+
+#include <gdk/gdk.h>
+#include <gtk/gtk.h>
+
+#include "nautilus-dropbox.h"
+
+static GType type_list[1];
+
+void
+nautilus_module_initialize (GTypeModule *module) {
+  g_print ("Initializing %s\n", PACKAGE_STRING);
+
+  nautilus_dropbox_register_type (module);
+  type_list[0] = NAUTILUS_TYPE_DROPBOX;
+
+  dropbox_use_nautilus_submenu_workaround
+    = (NAUTILUS_VERSION_MAJOR < 2 ||
+       (NAUTILUS_VERSION_MAJOR == 2 && NAUTILUS_VERSION_MINOR <= 22));
+  dropbox_use_operation_in_progress_workaround = TRUE;
+}
+
+void
+nautilus_module_shutdown (void) {
+  g_print ("Shutting down dropbox extension\n");
+}
+
+void
+nautilus_module_list_types (const GType **types,
+                            int *num_types) {
+  *types = type_list;
+  *num_types = G_N_ELEMENTS (type_list);
+}
---
+++ src/nautilus-dropbox.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2008 Evenflow, Inc.
+ *
+ * nautilus-dropbox.h
+ * Header file for nautilus-dropbox.c
+ *
+ * This file is part of nautilus-dropbox.
+ *
+ * nautilus-dropbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nautilus-dropbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with nautilus-dropbox.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef NAUTILUS_DROPBOX_H
+#define NAUTILUS_DROPBOX_H
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <libnautilus-extension/nautilus-info-provider.h>
+
+#include "dropbox-command-client.h"
+#include "nautilus-dropbox-hooks.h"
+#include "dropbox-client.h"
+
+G_BEGIN_DECLS
+
+/* Declarations for the dropbox extension object.  This object will be
+ * instantiated by nautilus.  It implements the GInterfaces 
+ * exported by libnautilus. */
+
+#define NAUTILUS_TYPE_DROPBOX	  (nautilus_dropbox_get_type ())
+#define NAUTILUS_DROPBOX(o)	  (G_TYPE_CHECK_INSTANCE_CAST ((o), NAUTILUS_TYPE_DROPBOX, NautilusDropbox))
+#define NAUTILUS_IS_DROPBOX(o)	  (G_TYPE_CHECK_INSTANCE_TYPE ((o), NAUTILUS_TYPE_DROPBOX))
+typedef struct _NautilusDropbox      NautilusDropbox;
+typedef struct _NautilusDropboxClass NautilusDropboxClass;
+
+struct _NautilusDropbox {
+  GObject parent_slot;
+  GHashTable *filename2obj;
+  GHashTable *obj2filename;
+  GMutex *emblem_paths_mutex;
+  GHashTable *emblem_paths;
+  DropboxClient dc;
+};
+
+struct _NautilusDropboxClass {
+	GObjectClass parent_slot;
+};
+
+GType nautilus_dropbox_get_type(void);
+void  nautilus_dropbox_register_type(GTypeModule *module);
+
+extern gboolean dropbox_use_nautilus_submenu_workaround;
+extern gboolean dropbox_use_operation_in_progress_workaround;
+
+G_END_DECLS
+
+#endif
